99cb7c60c354d61f634ac1a19f884f9e4de3c26b
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2022-05-05 15:16:50 +0100

    Modify controllers & view

---
+++ test/controllers/expense_categories_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class ExpenseCategoriesControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get expense_categories_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get expense_categories_index_url
+    assert_response :success
+  end
+end
